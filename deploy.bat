1e5ae6d95bfd6b9a3c015e525be6b882bf26141e
Siam Hasan <hasan.2017.siam@gmail.com>
2023-07-16 04:18:16 +0600

    Sun 07/16/2023  4:18:16.57

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,21 @@
-“@ECHO OFF
+@ECHO OFF
+:: Step 1: Move index.html to root directory
 MOVE .\playwright-report\index.html .
-git add .
-git commit -m "%date%-%time%"
+if %ERRORLEVEL% EQU 0 (
+    echo "Successfully moved index.html to root directory"
+) else (
+    echo "Failed to move index.html to root directory"
+    exit /b 1
+)
 
+:: Step 2: Commit and push the changes to GitHub
+set commit_message=%date% %time%
+git add index.html deploy.bat
+git commit -m "%commit_message%"
+git push origin main
+if %ERRORLEVEL% EQU 0 (
+    echo "Successfully pushed changes to GitHub"
+) else (
+    echo "Failed to push changes to GitHub"
+    exit /b 1
+)
